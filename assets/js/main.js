b73ad1ad4d1e00ac4675588b0190555da72713ee
Mohammed Afzaal <mohdafzaal71@gmail.com>
2023-10-14 10:53:48 +0300

    InitialCreate

---
+++ assets/js/main.js
@@ -0,0 +1,62 @@
+$(document).ready(function () {
+  $('.sidebar-toggler').click(function (e) { 
+    e.preventDefault();
+    $('main').toggleClass('sb-active');  
+  });
+  $('.left-side-overlay').click(function (e) { 
+    e.preventDefault();
+    $('main').removeClass('sb-active');  
+  });
+  
+});
+
+/* Video Play on Click */
+const video = document.getElementById("video");
+const circlePlayButton = document.getElementById("circle-play-b");
+
+function togglePlay() {
+	if (video.paused || video.ended) {
+		video.play();
+	} else {
+		video.pause();
+	}
+}
+
+circlePlayButton.addEventListener("click", togglePlay);
+video.addEventListener("playing", function () {
+	circlePlayButton.style.opacity = 0;
+});
+video.addEventListener("pause", function () {
+	circlePlayButton.style.opacity = 1;
+});
+
+jQuery(".message-slider").owlCarousel({
+  autoplay: true,
+  rewind: true, /* use rewind if you don't want loop */
+  margin: 20,
+   /*
+  animateOut: 'fadeOut',
+  animateIn: 'fadeIn',
+  */
+  responsiveClass: true,
+  autoplayTimeout: 7000,
+  smartSpeed: 800,
+  nav: false,
+  responsive: {
+    0: {
+      items: 1
+    },
+
+    600: {
+      items: 1
+    },
+
+    1024: {
+      items: 1
+    },
+
+    1366: {
+      items: 1
+    }
+  }
+})
